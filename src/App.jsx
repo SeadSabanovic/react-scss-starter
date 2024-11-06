c8bc70e476999e3978bf67efe4d6ff8cafcb1d62
Sead Šabanović <sead.sabanovic97@gmail.com>
2024-11-06 22:04:24 +0100

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/app.scss';
+
+function App() {
+  return (
+    <div className="home">
+      <div className="container">
+        <h1>Hello World</h1>
+        <button className="button">Click me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
